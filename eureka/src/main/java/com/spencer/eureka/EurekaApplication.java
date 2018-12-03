ac34865447c0ce1c9b56cf8782c35c2d9f8cfe65
Lichao <lichao48@guazi.com>
2018-12-03 18:14:35 +0800

    eureka init

---
+++ eureka/src/main/java/com/spencer/eureka/EurekaApplication.java
@@ -0,0 +1,32 @@
+package com.spencer.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * @author lichao
+ * @date 2018/12/3
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
+
+}
